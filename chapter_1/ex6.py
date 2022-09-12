d0ed387e9ffdd301c69e4686644af57e89d31932
koshkink1002 <koshkink1002@gmail.com>
2022-09-12 22:40:39 +0300

    Сделал вывод всех переменных

--- chapter_1/ex6.py
+++ chapter_1/ex6.py
@@ -3,3 +3,6 @@ price: float = float(input("Введите сумму заказа "))
 tips = 100 * 18 / price
 tax = 100 * 4 / price
 
+print(f"Общая цена - {price} ")
+print(f"Чаевые - {tips }")
+print(f"Налог - {tax} ")
